7c83c1554d0a2c0bad896f5b14db70922cd7c9a9
Meteosource <support@meteosource.com>
2021-09-15 17:33:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
-import os
 from setuptools import find_packages, setup
 
-
-with open(os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), "README.md")) as f:
-    README = f.read()
 
 setup(
     version="0.1",
@@ -13,7 +8,6 @@ setup(
     install_requires=["wheel", "requests", "pytz"],
     extras_require={"pandas": "pandas"},
     description="Meteosource API wrapper library",
-    long_description=README,
     author="Meteosource",
     author_email="support@meteosource.com",
     url="https://github.com/Meteosource/pymeteosource/archive/v_01.tar.gz",
